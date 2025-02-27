39757cdc64723553b7f5a4c66db6872d2b8d0f90
c-antin <38494182+c-antin@users.noreply.github.com>
2025-02-27 10:33:02 +0100

    AssignmentExpression

---
+++ lint-plugin.test.ts
@@ -0,0 +1,28 @@
+import { assert, assertEquals } from "jsr:@std/assert";
+import plugin, { to_message } from "./lint-plugin.ts";
+
+const ID = "lint-plugin-rust-style/rust-style";
+
+Deno.test("valid assignment", () => {
+  const diagnostics = Deno.lint.runPlugin(
+    plugin,
+    "main.ts",
+    'valid_assignment = "Ichigo";',
+  );
+
+  assertEquals(diagnostics.length, 0);
+});
+
+Deno.test("invalid assignment", () => {
+  const diagnostics = Deno.lint.runPlugin(
+    plugin,
+    "main.ts",
+    'invalidAssignment = "Ichigo";',
+  );
+
+  assertEquals(diagnostics.length, 1);
+  const d = diagnostics[0];
+  assertEquals(d.id, ID);
+  assertEquals(d.message, to_message("invalidAssignment"));
+  assert(typeof d.fix !== "undefined");
+});
--- lint-plugin.ts
+++ lint-plugin.ts
@@ -1,9 +1,165 @@
+// loosely based on DestructuringFinder https://github.com/swc-project/swc/blob/e74929c01d2d8b9001bbc056f20ca8e1cb1c9a63/crates/swc_ecma_utils/src/lib.rs#L1520
+const find_lhs_pat_ids = (
+  arr: (
+    | Deno.lint.Identifier
+    | Deno.lint.ArrayPattern
+    | Deno.lint.ObjectPattern
+    | Deno.lint.MemberExpression
+    | Deno.lint.AssignmentPattern
+    | Deno.lint.RestElement
+    | Deno.lint.Property
+    | null
+  )[],
+): Deno.lint.Identifier[] => {
+  return arr.flatMap((n) => {
+    if (n === null) return [];
+    switch (n.type) {
+      case "Identifier":
+        return [n];
+      case "ArrayPattern":
+        return find_lhs_pat_ids(n.elements);
+      case "ObjectPattern":
+        return find_lhs_pat_ids(n.properties);
+      case "MemberExpression":
+        //ignore member expression for now
+        return [];
+      case "AssignmentPattern":
+        return find_lhs_ids(n.left);
+      case "RestElement":
+        return find_lhs_pat_ids([n.argument]);
+      case "Property": {
+        //ignore key
+        switch (n.value.type) {
+          case "AssignmentPattern":
+            return find_lhs_ids(n.value.left);
+          case "TSEmptyBodyFunctionExpression":
+            return [];
+          default:
+            return find_lhs_ids(n.value);
+        }
+      }
+    }
+  });
+};
+
+const find_lhs_ids = (n: Deno.lint.Expression): Deno.lint.Identifier[] => {
+  switch (n.type) {
+    case "Identifier":
+      return [n];
+    case "ArrayPattern":
+      return find_lhs_pat_ids(n.elements);
+    case "ObjectPattern":
+      return find_lhs_pat_ids(n.properties);
+  }
+  return [];
+};
+
+const is_snake_cased = (ident_name: string) => {
+  return !/[A-Z]/.test(ident_name);
+};
+
+const is_screaming_snake_cased = (ident_name: string) => {
+  return !/[a-z]$/.test(ident_name);
+};
+
+const to_snake_case = (ident_name: string) => {
+  const result = ident_name.replaceAll(
+    /([a-z])([A-Z])/g,
+    (_match, caps1, caps2) => {
+      return `${caps1}_${caps2.toLowerCase()}`;
+    },
+  );
+
+  if (result !== ident_name) {
+    return result.toLowerCase();
+  }
+
+  return ident_name.toLowerCase();
+};
+
+type IdentToCheck =
+  | "variable"
+  | "variable_or_constant"
+  | "object_key"
+  | "function"
+  | "function_or_component"
+  | "class"
+  | "type_alias"
+  | "interface"
+  | "enum_name";
+
+export const to_message = (
+  name: string,
+) => {
+  return `Identifier '${name}' is not in rust style.`;
+};
+
+const to_hint = (
+  name: string,
+  ident_type: IdentToCheck,
+) => {
+  switch (ident_type) {
+    case "variable":
+      return `Consider renaming \`${name}\` to \`${to_snake_case(name)}\``;
+    case "variable_or_constant": {
+      const snake_cased = to_snake_case(name);
+      return `Consider renaming \`${name}\` to \`${snake_cased}\` or \`${snake_cased.toUpperCase()}\``;
+    }
+  }
+};
+
+const check_ident_snake_cased = (
+  id: Deno.lint.Identifier,
+  context: Deno.lint.RuleContext,
+  ident_type: IdentToCheck,
+) => {
+  const node = id;
+  if (is_snake_cased(node.name)) return;
+  context.report({
+    node,
+    message: to_message(node.name),
+    hint: to_hint(node.name, ident_type) ?? "",
+    fix(fixer) {
+      // const original = context.sourceCode.getText(node);
+      // const newText = `{ ${original} }`;
+      return fixer.replaceText(node, to_snake_case(node.name));
+    },
+  });
+};
+
+const check_ident_snake_cased_or_screaming_snake_cased = (
+  id: Deno.lint.Identifier,
+  context: Deno.lint.RuleContext,
+  ident_type: IdentToCheck,
+) => {
+  const node = id;
+  if (is_snake_cased(node.name)) return;
+  if (is_screaming_snake_cased(node.name)) return;
+  context.report({
+    node,
+    message: to_message(node.name),
+    hint: to_hint(node.name, ident_type) ?? "",
+    fix(fixer) {
+      // const original = context.sourceCode.getText(node);
+      // const newText = `{ ${original} }`;
+      return fixer.replaceText(node, to_snake_case(node.name));
+    },
+  });
+};
+
 export default {
   name: "lint-plugin-rust-style",
   rules: {
     "rust-style": {
-      create(_context) {
-        return {};
+      create(context) {
+        return {
+          AssignmentExpression(node) {
+            const ids = find_lhs_ids(node.left);
+            for (const id of ids) {
+              check_ident_snake_cased(id, context, "variable");
+            }
+          },
+        };
       },
     },
   },
